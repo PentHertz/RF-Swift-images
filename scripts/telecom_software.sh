2ae3ab4ad9dafeca660ec63d6dbf8808cab746f0
FlUxIuS <fluxius@gmail.com>
2024-11-06 18:31:03 +0100

    Forcing meson install for RISCV-64

--- scripts/telecom_software.sh
+++ scripts/telecom_software.sh
@@ -160,8 +160,8 @@ function srsran5GSA_soft_install() {
 
 function Open5GS_soft_install() {
 	goodecho "[+] Installing Open5GS dependencies"
-	installfromnet "apt-fast -y update"
-	installfromnet "apt-fast -y install ca-certificates curl gnupg"
+	install_dependencies "ca-certificates curl gnupg"
+	install_dependencies "meson"
 	curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
 	echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 	installfromnet "apt-fast -y update"
