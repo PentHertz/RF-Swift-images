0df028c7bf6fd4890aa3e84b1e32a682a6d1fb58
FlUxIuS <fluxius@gmail.com>
2024-11-08 17:08:03 +0100

    Fixing telecom missing deps

--- scripts/telecom_software.sh
+++ scripts/telecom_software.sh
@@ -162,6 +162,7 @@ function Open5GS_soft_install() {
 	goodecho "[+] Installing Open5GS dependencies"
 	install_dependencies "ca-certificates curl gnupg"
 	install_dependencies "meson libmongoc-1.0-0 libmongoc-dev"
+	install_dependencies "python3-pip python3-setuptools python3-wheel ninja-build build-essential flex bison git cmake libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libnghttp2-dev libmicrohttpd-dev libcurl4-gnutls-dev libnghttp2-dev libtins-dev libtalloc-dev meson"
 	ldconfig
 	curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
 	echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
