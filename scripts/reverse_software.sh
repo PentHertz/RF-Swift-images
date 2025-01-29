fb20cb4fd8bb62912954b080ceb00f95bbfa4142
FlUxIuS <fluxius@gmail.com>
2025-01-29 22:45:42 +0100

    Fixing some install for revere and bluetooth

--- scripts/reverse_software.sh
+++ scripts/reverse_software.sh
@@ -89,7 +89,8 @@ function ghidra_soft_install() {
 	cd /reverse
 
     ghidra_version="11.2.1"
-	prog="ghidra_${ghidra_version}_PUBLIC_20240709"
+    ghidra_date="20241105"
+	prog="ghidra_${ghidra_version}_PUBLIC_${ghidra_date}"
 
 	installfromnet "wget https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_${ghidra_version}_build/${prog}.zip"
 	unzip "$prog"
