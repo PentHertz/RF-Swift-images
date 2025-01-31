30028c086408c3ee6ca6c496a38f19ac1bd4d31c
FlUxIuS <fluxius@gmail.com>
2025-01-31 01:28:33 +0100

    libc6 install

--- scripts/corebuild.sh
+++ scripts/corebuild.sh
@@ -39,7 +39,7 @@ function docker_preinstall() {
         pulseaudio-utils libasound2-dev libavahi-client-dev task-lxqt-desktop
         language-pack-en libqwt-qt5-dev python3-click-plugins python3-zmq rsync
         iw wireless-tools usbutils bluetooth bluez bluez-tools rfkill avahi-daemon
-        qt6-base-dev qt6-base-dev-tools qt6-tools-dev qt6-tools-dev-tools
+        qt6-base-dev qt6-base-dev-tools qt6-tools-dev qt6-tools-dev-tools libc6-dev
     )
 
     # creating a symblink for python3 for some requirements
