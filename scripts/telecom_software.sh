7481b498539542435b9997caabb11e4ada952a2c
FlUxIuS <fluxius@gmail.com>
2025-05-23 20:08:11 +0200

    Adding Open5Gs without HTTP2

--- scripts/telecom_software.sh
+++ scripts/telecom_software.sh
@@ -213,6 +213,40 @@ function Open5GS_soft_install() {
 	npm ci
 }
 
+
+function Open5GS_nohttp2_soft_install() {
+	goodecho "[+] Installing Open5GS dependencies"
+	install_dependencies "ca-certificates curl gnupg"
+	install_dependencies "meson libmongoc-1.0-0 libmongoc-dev"
+	install_dependencies "python3-pip python3-setuptools python3-wheel ninja-build build-essential flex bison git cmake libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libnghttp2-dev libmicrohttpd-dev libcurl4-gnutls-dev libnghttp2-dev libtins-dev libtalloc-dev meson"
+	ldconfig
+	curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
+	echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+	installfromnet "apt-fast -y update"
+	install_dependencies "mongodb-org python3-pip python3-setuptools python3-wheel ninja-build build-essential flex bison git cmake libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libnghttp2-dev libmicrohttpd-dev libcurl4-gnutls-dev libnghttp2-dev libtins-dev libtalloc-dev meson"
+	goodecho "[+] Feching Open5GS"
+	[ -d /telecom/5G ] || mkdir -p /telecom/5G
+	cd /telecom/5G
+	goodecho "[+] Cloninig and installing Open5GS"
+	installfromnet "git clone -b nohttp2 https://github.com/FlUxIuS/open5gs.git"
+	cd open5gs
+	meson build --prefix=`pwd`/install
+	ninja -C build
+	ln -s $(pwd)/build/tests/app/5gc /usr/bin/Open5Gs_deployall # Making a quick command
+	mkdir -p /data/db # making directory for MongoDB
+	goodecho "[+] Building Web GUI"
+	mkdir -p /etc/apt/keyrings
+	curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+	NODE_MAJOR=20
+	echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+	installfromnet "apt-fast update"
+	install_dependencies "nodejs"
+	cd webui
+	npm ci
+}
+
+
+
 function pycrate_soft_install() {
     goodecho "[+] Installing pycrate"
     pip3install "pycrate"
