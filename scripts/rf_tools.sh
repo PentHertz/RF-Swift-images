b3cd6f30aadbb7a048a190d713c9fcd80ce1083b
FlUxIuS <fluxius@gmail.com>
2025-07-11 09:13:41 +0200

    Fixing WPA3 Dragondrain

--- scripts/rf_tools.sh
+++ scripts/rf_tools.sh
@@ -363,6 +363,7 @@ function wpa3_dragondrain_and_time_soft_install() {
 	cd /rftools/wifi/wpa3
 	install_dependencies "autoconf automake libtool shtool libssl-dev pkg-config"
 	gitinstall "https://github.com/vanhoefm/dragondrain-and-time.git" "wpa3_dragondrain_and_time_soft_install"
+	cd dragondrain-and-time
 	autoreconf -i
 	./configure
 	make -j$(nproc)
