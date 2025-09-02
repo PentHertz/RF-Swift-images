dbbbc8d999686d77b9b733d5eeaa74bcddbb2567
FlUxIuS <fluxius@gmail.com>
2025-09-02 20:15:14 +0200

    Completing ZigBee install by installing HBs

--- scripts/gr_oot_modules.sh
+++ scripts/gr_oot_modules.sh
@@ -164,6 +164,11 @@ function grieee802-11_grmod_install() {
 
 function grieee802154_grmod_install() {
     grclone_and_build "https://github.com/bastibl/gr-ieee802-15-4.git" "" "grieee802154_grmod_install"
+    cd /rftools/sdr/oot/gr-ieee802-15-4/examples
+    grc *.grc
+    mkdir -p /root/.grc_gnuradio
+    cp *.yml /root/.grc_gnuradio/
+    cp *.py /root/.grc_gnuradio/
 }
 
 function grrds_grmod_install() {
