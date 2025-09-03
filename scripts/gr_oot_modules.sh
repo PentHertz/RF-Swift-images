1e54c87b729c38d57089fa32c591f86e0b4d80b2
FlUxIuS <fluxius@gmail.com>
2025-09-03 21:47:37 +0200

    Adding RF Tap OOT

--- scripts/gr_oot_modules.sh
+++ scripts/gr_oot_modules.sh
@@ -363,3 +363,7 @@ function grflarm_grmod_install() {
 function grguiextra_grmod_install() {
     grclone_and_build "https://github.com/ghostop14/gr-guiextra.git" "" "grguiextra_grmod_install"
 }
+
+function grrftap_grmod_install() {
+    grclone_and_build "https://github.com/FlUxIuS/gr-rftap.git" "" "grrftap_grmod_install"
+}
