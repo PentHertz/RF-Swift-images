52ec454c0a9a12b8947e032a13c9094212788ba0
FlUxIuS <fluxius@gmail.com>
2026-05-17 22:05:35 +0200

    Updating Little Snitch for Linux

--- scripts/corebuild.sh
+++ scripts/corebuild.sh
@@ -273,7 +273,7 @@ function uvpython_install() { # Avoid terrible long builds
 
 function littlesnitch_soft_install() {
     local arch
-    local version="1.0.8"
+    local version="1.0.9"
     arch="$(uname -m)"
     case "$arch" in
         x86_64|amd64)
